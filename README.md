63641c7232c9f9bb7b26c488aa9d7d59c099ab17
Riccardo Torrisi <r.torrisi96@gmail.com>
2018-12-21 21:30:44 +0100

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-rtorrisi `GitHub Page`
---
+++ precache-manifest.6fdfbdba75fc56befb617c135bb7fda4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "99a1c3a3ef938b6d56be",
+    "url": "/static/js/main.99a1c3a3.chunk.js"
+  },
+  {
+    "revision": "0651a7edae8ffb58a867",
+    "url": "/static/js/1.0651a7ed.chunk.js"
+  },
+  {
+    "revision": "99a1c3a3ef938b6d56be",
+    "url": "/static/css/main.dacd4013.chunk.css"
+  },
+  {
+    "revision": "45b012933ec9e484c7ecb237d96a930b",
+    "url": "/index.html"
+  }
+];
